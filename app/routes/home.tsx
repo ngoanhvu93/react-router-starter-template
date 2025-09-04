976a7abc4933ecfd24b8018f00671eef3f02ee45
Vu Ngo <vu_ngo@fastboy.net>
2025-09-04 17:33:21 +0700

    Refactor Home component to remove message prop from Welcome; update import statement for consistency.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
+import Welcome from "~/welcome/welcome";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,5 +13,5 @@ export function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <Welcome />;
 }
